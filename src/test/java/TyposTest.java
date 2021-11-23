67d2c21c7caeb70f429a5a827e46d40e1a43e50b
Aleksandra <taeyeononni@gmail.com>
2021-11-23 14:42:11 +0300

    Second add

--- src/test/java/TyposTest.java
+++ src/test/java/TyposTest.java
@@ -12,6 +12,6 @@ public class TyposTest extends BaseTest {
         List<WebElement> typo = driver.findElements(By.tagName("p"));
 
         Assert.assertEquals(typo.get(0).getText(), "This example demonstrates a typo being introduced. It does it randomly on each page load.", "Error on the first 'p'");
-        Assert.assertEquals(typo.get(1).getText(), "Sometimes you'll see a typo, other times you wont.", "Error on the second 'p'");
+        Assert.assertEquals(typo.get(1).getText(), "Sometimes you'll see a typo, other times you won't.", "Error on the second 'p'");
     }
 }
